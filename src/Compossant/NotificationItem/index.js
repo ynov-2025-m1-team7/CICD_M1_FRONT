b5eb346f9d0a95150c8c1cab9ac8d20f95958da8
Ibtissam78 <ibtissamelhany.pro@gmail.com>
2025-05-05 14:35:07 +0200

    notificationv1

--- src/Compossant/NotificationItem/index.js
+++ src/Compossant/NotificationItem/index.js
@@ -1,21 +1,12 @@
 import React from "react";
+import "./style.css";
 
 const NotificationItem = ({ date, message }) => {
   return (
-    <div style={styles.item}>
+    <div className="item">
       <strong>{date}:</strong> {message}
     </div>
   );
-};
-
-const styles = {
-  item: {
-    background: "#fff",
-    padding: "10px",
-    marginBottom: "10px",
-    borderRadius: "5px",
-    boxShadow: "0px 0px 5px rgba(0,0,0,0.1)"
-  }
 };
 
 export default NotificationItem;
