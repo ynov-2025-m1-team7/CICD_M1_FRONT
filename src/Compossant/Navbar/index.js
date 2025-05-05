591ac12a21b21f21801ce331c909daae8d2ab096
Pepito Mercurio <coco.run91@gmail.com>
2025-05-05 11:59:47 +0200

    navigate

--- src/Compossant/Navbar/index.js
+++ src/Compossant/Navbar/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 import NavButton from "../NavButton";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate();
     return (
         <div className="navbar">
             <div className="navbar__links">
-                <NavButton text={"Dashboard"} image={'/assets/home.svg'} imageSelected={'/assets/home_selected.svg'} id={1} redirection={"/"} onClick={() => {}}/>
-                <NavButton text={"Feedback"} image={'/assets/bell.svg'} imageSelected={'/assets/bell.svg'} id={2} redirection={"feedback"} onClick={() => {}}/>
+                <NavButton text={"Dashboard"} image={'/assets/home.svg'} imageSelected={'/assets/home_selected.svg'} id={1} redirection={"/"} onClick={() => {navigate("/")}}/>
+                <NavButton text={"Feedback"} image={'/assets/bell.svg'} imageSelected={'/assets/bell.svg'} id={2} redirection={"feedback"} onClick={() => {navigate("/feedback")}}/>
                 <NavButton text={"Notification"} image={'/assets/message.svg'} imageSelected={'/assets/message_selected.svg'} id={3} onClick={() => {}}/>
             </div>
         </div>
