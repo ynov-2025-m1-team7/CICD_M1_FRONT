c81b59141f8374135fa3151f4eb3c9a13119c079
François-Alexian Lopes <francois.lopes@yahoo.com>
2025-05-05 14:35:50 +0200

    resolve black active page navbar

--- src/Compossant/Navbar/index.js
+++ src/Compossant/Navbar/index.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="navbar__links">
                 <NavButton text={"Dashboard"} image={'/assets/home.svg'} imageSelected={'/assets/home_selected.svg'} id={1} redirection={"/"} onClick={() => {navigate("/")}}/>
-                <NavButton text={"Feedback"} image={'/assets/bell.svg'} imageSelected={'/assets/bell.svg'} id={2} redirection={"feedback"} onClick={() => {navigate("/feedback")}}/>
+                <NavButton text={"Feedback"} image={'/assets/bell.svg'} imageSelected={'/assets/bell.svg'} id={2} redirection={"/feedback"} onClick={() => {navigate("/feedback")}}/>
                 <NavButton text={"Notification"} image={'/assets/message.svg'} imageSelected={'/assets/message_selected.svg'} id={3} onClick={() => {}}/>
             </div>
         </div>
