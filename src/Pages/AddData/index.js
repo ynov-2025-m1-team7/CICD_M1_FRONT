37dc7f5c886a77b52136b1e341bc6f8f1c474530
Pepito Mercurio <coco.run91@gmail.com>
2025-05-05 16:43:33 +0200

    fix addData

--- src/Pages/AddData/index.js
+++ src/Pages/AddData/index.js
@@ -1,12 +1,16 @@
+import Header from "../../Compossant/Header";
 import IdentificationBanner from "../../Compossant/IdentificationBanner";
 import JsonForm from "../../Compossant/JsonForm";
 import './style.css';
 
 const JsonFormPage = () => {
     return (
-        <div className="json-form-container">
-            <IdentificationBanner title={"Ajouter des données"} />
-            <JsonForm />
+        <div className="json-form-page">
+            <Header />
+            <div className="json-form-container">
+                <IdentificationBanner title={"Ajouter des données"} />
+                <JsonForm />
+            </div>
         </div>
     );
 }
