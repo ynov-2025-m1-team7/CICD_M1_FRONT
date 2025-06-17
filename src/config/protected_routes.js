4d94e2a2db20afb807608c3b06760fc42b560826
Pepito Mercurio <coco.run91@gmail.com>
2025-06-17 14:47:36 +0200

    one more thing

--- src/config/protected_routes.js
+++ src/config/protected_routes.js
@@ -4,6 +4,10 @@ import Cookies from "js-cookie";
 const ProtectedRoute = ({ children }) => {
   const token = Cookies.get("jwt_token");
 
+  if (!children) {
+    throw new Error("Children prop is required for ProtectedRoute");
+  }
+
   if (!token) {
     return <Navigate to="/signin" replace />;
   }
